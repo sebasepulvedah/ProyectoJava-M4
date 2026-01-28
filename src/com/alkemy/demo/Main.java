750d548b045a84bfc86932210795ce6d88ba42f5
Sebastián Sepúlveda <seba.sepulvedah@gmail.com>
2026-01-28 12:03:06 -0300

    probando Java en Eclipse

---
+++ src/com/alkemy/demo/Main.java
@@ -0,0 +1,14 @@
+package com.alkemy.demo;
+
+public class Main {
+
+	public static void main(String[] args) { // esto es un metodo
+		// TODO Auto-generated method stub
+			System.out.println("Hola Clase, VAMOS CON TODO!"); // este es el codigo.
+			System.err.println("Esto es un error");
+			System.out.println();
+			System.out.print(0);
+			System.out.println("Main.main");
+	}
+
+}
